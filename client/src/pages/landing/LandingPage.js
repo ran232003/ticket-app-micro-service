06402a6a8dc1efc374f7060bd621c0dbece9413d
Ran Farjun <mike232003@gmail.com>
2023-07-11 17:42:38 +0300

    update common

--- client/src/pages/landing/LandingPage.js
+++ client/src/pages/landing/LandingPage.js
@@ -1,15 +1,12 @@
 import React, { useEffect } from "react";
 import { getCurrentUser } from "../../apiCalls";
+import { useSelector } from "react-redux";
 
 const LandingPage = (props) => {
-  //   const getUser = async () => {
-  //     const data = await getCurrentUser();
-  //     console.log(data);
-  //   };
-  //   useEffect(() => {
-  //     getUser();
-  //   }, []);
-  return <div>landing</div>;
+  const user = useSelector((state) => {
+    return state.auth.user;
+  });
+  return <div>landing {user.email}</div>;
 };
 
 export default LandingPage;
